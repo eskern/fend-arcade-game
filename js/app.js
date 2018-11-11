790ddfc53287292eb3f86e47d3889956e6e47525
Emily Sarah Kern <emily.kern28@gmail.com>
2018-11-11 10:43:28 -0500

    updated readme (my bad, totally forgot) and fixed win condition so that the player actually gets in the water to win; also fixed indentation

--- js/app.js
+++ js/app.js
@@ -14,20 +14,20 @@ class Enemy{
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    if(this.x < this.boundary){ //enemy not out of bounds
-      this.x += this.speed*dt;
-    }
-    else{
-      this.x = this.resetPos; //reset enemy position
-    }
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  if(this.x < this.boundary){ //enemy not out of bounds
+    this.x += this.speed*dt;
+  }
+  else{
+    this.x = this.resetPos; //reset enemy position
+  }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
@@ -53,9 +53,10 @@ class Hero {
         (enemy.x+enemy.step/2 > this.x &&
           enemy.x < this.x + this.step/2)){
         this.reset();
+        console.log(this.y);
       }
     }
-    if(this.y === 55){ //hero made it to the final tile (water) at y=55
+    if(this.y === -28){ //hero made it to the final tile (water)
       this.victory = true;
     }
   }
@@ -79,8 +80,11 @@ class Hero {
        }
      }
      else if(input == 'up'){
-       if(this.y > this.jump){
+       if(this.y > 0){
          this.y -= this.jump;
+       }
+       else{
+         console.log(this.y)
        }
      }
      else if(input == 'down'){
@@ -91,13 +95,17 @@ class Hero {
    }
 }
 
+function randNum(){
+  return (Math.random() * 400) + 100;
+}
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 const player = new Hero();
-const bug1 = new Enemy(-101, 0, 200);
-const bug2 = new Enemy(-101, 83, 300);
-const bug3 = new Enemy(-101*2.5, 83, 300);
+const bug1 = new Enemy(-101, 166, randNum());
+const bug2 = new Enemy(-101, 83, randNum());
+const bug3 = new Enemy(-101*2.5, 83, randNum());
 const allEnemies = [];
 allEnemies.push(bug1);
 allEnemies.push(bug2);
@@ -106,12 +114,11 @@ allEnemies.push(bug3);
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+  var allowedKeys = {
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down'
+  };
+  player.handleInput(allowedKeys[e.keyCode]);
 });
